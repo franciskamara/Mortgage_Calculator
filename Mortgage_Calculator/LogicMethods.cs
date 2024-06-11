8b426f85ee2263564044c74adff6454fdd481c17
Francis Kamara <franciskamara@hotmail.co.uk>
2024-06-11 14:12:27 +0100

    WIP: Mortg Calc

--- Mortgage_Calculator/LogicMethods.cs
+++ Mortgage_Calculator/LogicMethods.cs
@@ -5,16 +5,25 @@ namespace Mortgage_Calculator
 	{
         public static Results CalcResults(UserInput input, MortgageType t)
         {
+            var totalAmount = input.Amount;
+            var interestRate = input.InterestRate;
+            var termYears = input.Term;
 
-            if(t == MortgageType.Repayment)
+            if(t == MortgageType.Interest_Only)
             {
                 //todo implement first the one thats easer
+                var interestPayment = ((totalAmount * interestRate) / CONSTANTS.ONE_YEAR) * termYears;
+
+                
             }
-            else
+            if(t == MortgageType.Repayment)
             {
                 //implement other calc
+                var fullRepayment = totalAmount * (interestRate * CONSTANTS.ONE_YEAR) /
+                    1 - (1 + (interestRate * CONSTANTS.ONE_YEAR)) - CONSTANTS.ONE_YEAR * termYears;
             }
 
+            return 
             //TODO Implement
 
             //Interest-only payment calc
