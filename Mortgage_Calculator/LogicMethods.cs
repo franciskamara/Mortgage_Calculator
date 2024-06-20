6a79df49fbe57eef5f42dabaaffe287dc549a7f2
Francis Kamara <franciskamara@hotmail.co.uk>
2024-06-20 21:27:14 +0100

    WIP: Calc code clean-up

--- Mortgage_Calculator/LogicMethods.cs
+++ Mortgage_Calculator/LogicMethods.cs
@@ -8,43 +8,32 @@ namespace Mortgage_Calculator
             int totalAmount = input.Amount;
             double interestRateDecimal = input.InterestRatePercentage / 100;
             int termYears = input.Term;
-            List<double> repayments = new List<double>();
+            int totalMonths = termYears * CONSTANTS.TWELVE_MONTHS;//Total months of Loan term calc
+            double monthlyRepayment;
+            List<double> repayments = new();
 
             if (t == MortgageType.Interest_Only)
             {
                 //Interest-only payment calc
                 //((Amount x InterestRate) / 12months) x totalMonths
-
-                double monthlyRepayment = (totalAmount * interestRateDecimal) / CONSTANTS.TWELVE_MONTHS;//Monthly repayment calc
-                int totalMonths = termYears * CONSTANTS.TWELVE_MONTHS;//Total months of Loan term calc
-                for (int monthly = 1; monthly <= totalMonths; monthly++)//Loop as many times totalMonths
-                {
-                    repayments.Add(monthlyRepayment);//List the month's repayment 
-                }
+                monthlyRepayment = (totalAmount * interestRateDecimal) / CONSTANTS.TWELVE_MONTHS;//Monthly repayment calc
             }
             else
             {
-                double monthlyInterestRate = interestRateDecimal / CONSTANTS.TWELVE_MONTHS;//Monthly interest rate calc
-                int totalMonths = termYears * CONSTANTS.TWELVE_MONTHS;//Total months of repayment calc
-                double monthlyRepayment = totalAmount * (monthlyInterestRate * Math.Pow(1 + monthlyInterestRate, totalMonths)) /
-                                  (Math.Pow(1 + monthlyInterestRate, totalMonths) - 1);
-                
-                for (int month = 1; month < totalMonths; month++)
-                {
-                    repayments.Add(monthlyRepayment);
-                }
-
                 //Repayment calc
                 //Repayment = Amount x (monthlyInterestRate x (1 + (monthlyInterestRate * 12months))) /
                 // (1 + (monthlyInterestRate x totalMonths) - 1)
-
-
+                double monthlyInterestRate = interestRateDecimal / CONSTANTS.TWELVE_MONTHS;//Monthly interest rate calc
+                monthlyRepayment = totalAmount * (monthlyInterestRate * Math.Pow(1 + monthlyInterestRate, totalMonths)) /
+                                  (Math.Pow(1 + monthlyInterestRate, totalMonths) - 1);//Standard repayment calc       
             }
+            for (int month = 1; month <= totalMonths; month++)//Loop as many times as totalMonths
+            {
+                repayments.Add(monthlyRepayment);//List the month repayments
+            }
+
             return new Results();
-            //{
-            //    //todo 
-            //};
-            
+
         }
     }
 }
