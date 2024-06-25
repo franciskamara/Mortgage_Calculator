625d9db8a91651a5d94366277bdfeffa77f952a5
Francis Kamara <franciskamara@hotmail.co.uk>
2024-06-25 14:15:34 +0100

    formatting

--- Mortgage_Calculator/userInput.cs
+++ Mortgage_Calculator/userInput.cs
@@ -1,42 +1,42 @@
 ﻿using System;
 namespace Mortgage_Calculator
 {
-	public class UserInput
-	{
-		
-		private int _amount;
-		public int Amount
-		{
-			get { return _amount; }
-			set { _amount = value; }
-		}
+    public class UserInput
+    {
 
-		private int _term;
-		public int Term
-		{
-			get { return _term; }
-			set { _term = value; }
-		}
+        private int _amount;
+        public int Amount
+        {
+            get { return _amount; }
+            set { _amount = value; }
+        }
 
-		private mortgageType _type;
-		public mortgageType Type
-		{
-			get { return _type; }
-			set { _type = value; }
-		}
+        private int _term;
+        public int Term
+        {
+            get { return _term; }
+            set { _term = value; }
+        }
 
-		private double _interestRate;
-		public double InterestRate
-		{
-			get { return _interestRate; }
-			set { _interestRate = value; }
-		}
+        private MortgageType _type;
+        public MortgageType Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
 
-		private double _deposit;
-		public double Deposit
-		{
-			get { return _deposit; }
-			set { _deposit = value; }
-		}
-	}
+        private double _interestRate;
+        public double InterestRatePercentage
+        {
+            get { return _interestRate; }
+            set { _interestRate = value; }
+        }
+
+        private double _deposit;
+        public double Deposit
+        {
+            get { return _deposit; }
+            set { _deposit = value; }
+        }
+    }
 }
