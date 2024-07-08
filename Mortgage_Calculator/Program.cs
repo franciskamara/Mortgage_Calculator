96e328cef6d7b742093717ca78df156c4d88e844
Francis Kamara <franciskamara@hotmail.co.uk>
2024-07-08 12:52:33 +0100

    Program - interestRate & Deposit inputs

--- Mortgage_Calculator/Program.cs
+++ Mortgage_Calculator/Program.cs
@@ -4,19 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the mortgage calculator \n");
 
             int mortgageCalcSelect = UIMethods.SingleOrDoubleCalculatorSelection();
 
             if (mortgageCalcSelect == CONSTANTS.OPTION_CALCULATE_SINGLE_MORTGAGE)
             {
-                
-                double loanAmount = UIMethods.LoanAmountInput();
+                UIMethods.PrintCalculateSingleMortgageMessage();
 
-                int termInput = UIMethods.TermTime();
+                UserInput inputs = new();//Call userInput class properties
+                MortgageType type = new();
+
+                inputs.Amount = UIMethods.LoanAmountInput();//Assign Amount input to userInput property: Amount
+
+                inputs.Term = UIMethods.TermTime();//Assign Term input to userInput property: Term
+
+                type.ToString();//enum placement !!!!
+                //UIMethods.MortgageType();
+
+                inputs.InterestRatePercentage = UIMethods.InterestRateInput();//Assign Interest Rate input to userInput property: Interest Rate
+
+                inputs.Deposit = UIMethods.DepositInput();//Assign Deposit input to userInput property: Deposit
+
             }
             if (mortgageCalcSelect == CONSTANTS.OPTION_CALCULATE_AND_COMPARE_TWO_MORTGAGES)
             {
+                UIMethods.PrintCalculateDoubleMortgageMessage();
+
 
             }
             
