41c57857376a62b08cccd3044b6ea21a531184b7
Francis Kamara <franciskamara@hotmail.co.uk>
2024-07-22 21:19:43 +0100

    BlazorApp project and Dependancy created

--- Mortgage_Calculator/LogicMethods.cs
+++ Mortgage_Calculator/LogicMethods.cs
@@ -8,7 +8,7 @@ namespace Mortgage_Calculator
             double totalAmount = input.Amount;
             double interestRateDecimal = input.InterestRatePercentage / 100;
             int termYears = input.Term;
-            int totalMonths = termYears * CONSTANTS.TWELVE_MONTHS;//Total months of Loan term calc
+            int totalMonths = termYears * CONSTANTS.MONTHS_OF_YEAR;//Total months of Loan term calc
             double monthlyRepayment;
 
             List<PaymentItem> repayments = new();
@@ -18,14 +18,14 @@ namespace Mortgage_Calculator
             {
                 //Interest-only monthly repayment calculation
                 //Interest-Only = ((Amount x InterestRate) / 12months) x totalMonths
-                monthlyRepayment = ((totalAmount * interestRateDecimal) / CONSTANTS.TWELVE_MONTHS);
+                monthlyRepayment = ((totalAmount * interestRateDecimal) / CONSTANTS.MONTHS_OF_YEAR);
             }
             else
             {
                 //Standard repayment calculation
                 //Repayment = Amount x (monthlyInterestRate x (1 + (monthlyInterestRate * 12months))) /
                 // (1 + (monthlyInterestRate x totalMonths) - 1)
-                double monthlyInterestRate = interestRateDecimal / CONSTANTS.TWELVE_MONTHS;//Monthly interest rate calc
+                double monthlyInterestRate = interestRateDecimal / CONSTANTS.MONTHS_OF_YEAR;//Monthly interest rate calc
                 monthlyRepayment = totalAmount * (monthlyInterestRate * Math.Pow(1 + monthlyInterestRate, totalMonths)) /
                                   (Math.Pow(1 + monthlyInterestRate, totalMonths) - 1);
             }
@@ -44,7 +44,7 @@ namespace Mortgage_Calculator
                     RemainingAmount = remainingAmount,
                 }); //Add data to Payment Items class
             }
-            double potentialIncreaseRepayment = monthlyRepayment * CONSTANTS.THREE_PERCENT;//Monthly repayment if interest rises by 3%
+            double potentialIncreaseRepayment = monthlyRepayment * CONSTANTS.INTEREST_RATE;//Monthly repayment if interest rises by 3%
             double roundedValue = Math.Round(potentialIncreaseRepayment, 2);
             Results results = new()//New results local variable
             {
