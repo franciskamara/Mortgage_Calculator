e4893d7cce54f853f56f8ed2508a4f779054b007
Francis Kamara <franciskamara@hotmail.co.uk>
2024-11-27 23:41:49 +0000

    WIP MortgageCalc.Specs, First Step

--- MortgageCalculator.Specs/Features/MortgageCalculator.feature
+++ MortgageCalculator.Specs/Features/MortgageCalculator.feature
@@ -6,11 +6,11 @@ Link to a feature: [Calculator]($projectname$/Features/Calculator.feature)
 ***Further read***: **[Learn more about how to generate Living Documentation](https://docs.specflow.org/projects/specflow-livingdoc/en/latest/LivingDocGenerator/Generating-Documentation.html)**
 
 @mytag
-Scenario: Add two numbers
-	Given the first number is 50
-	And the second number is 70
-	When the two numbers are added
-	Then the result should be 120
+#Scenario: Add two numbers
+#	Given the first number is 50
+#	And the second number is 70
+#	When the two numbers are added
+#	Then the result should be 120
 	
 Scenario: Click for Mortgage Calculator Nav
 	Given the user is on the homepage
--- MortgageCalculator.Specs/Steps/MortgageCalculatorStepDefinitions.cs
+++ MortgageCalculator.Specs/Steps/MortgageCalculatorStepDefinitions.cs
@@ -17,45 +17,45 @@ public sealed class MortgageCalculatorStepDefinitions
         _scenarioContext = scenarioContext;
     }
 
-    [Given("the first number is (.*)")]
-    public void GivenTheFirstNumberIs(int number)
-    {
-        //TODO: implement arrange (precondition) logic
-        // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
-        // To use the multiline text or the table argument of the scenario,
-        // additional string/Table parameters can be defined on the step definition
-        // method. 
-
-        _scenarioContext.Pending();
-    }
-
-    [Given("the second number is (.*)")]
-    public void GivenTheSecondNumberIs(int number)
-    {
-        //TODO: implement arrange (precondition) logic
-        // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
-        // To use the multiline text or the table argument of the scenario,
-        // additional string/Table parameters can be defined on the step definition
-        // method. 
-
-        _scenarioContext.Pending();
-    }
-
-    [When("the two numbers are added")]
-    public void WhenTheTwoNumbersAreAdded()
-    {
-        //TODO: implement act (action) logic
-
-        _scenarioContext.Pending();
-    }
-
-    [Then("the result should be (.*)")]
-    public void ThenTheResultShouldBe(int result)
-    {
-        //TODO: implement assert (verification) logic
-
-        _scenarioContext.Pending();
-    }
+    // [Given("the first number is (.*)")]
+    // public void GivenTheFirstNumberIs(int number)
+    // {
+    //     //TODO: implement arrange (precondition) logic
+    //     // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
+    //     // To use the multiline text or the table argument of the scenario,
+    //     // additional string/Table parameters can be defined on the step definition
+    //     // method. 
+    //
+    //     _scenarioContext.Pending();
+    // }
+    //
+    // [Given("the second number is (.*)")]
+    // public void GivenTheSecondNumberIs(int number)
+    // {
+    //     //TODO: implement arrange (precondition) logic
+    //     // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
+    //     // To use the multiline text or the table argument of the scenario,
+    //     // additional string/Table parameters can be defined on the step definition
+    //     // method. 
+    //
+    //     _scenarioContext.Pending();
+    // }
+    //
+    // [When("the two numbers are added")]
+    // public void WhenTheTwoNumbersAreAdded()
+    // {
+    //     //TODO: implement act (action) logic
+    //
+    //     _scenarioContext.Pending();
+    // }
+    //
+    // [Then("the result should be (.*)")]
+    // public void ThenTheResultShouldBe(int result)
+    // {
+    //     //TODO: implement assert (verification) logic
+    //
+    //     _scenarioContext.Pending();
+    // }
 
     //Mortgage Calculator homepage
     [Given("the user is on the homepage")]
