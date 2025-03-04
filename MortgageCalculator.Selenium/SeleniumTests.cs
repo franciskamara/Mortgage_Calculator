99db34a61f9a2ce519ab3ee336dcc80593f53a62
Francis Kamara <franciskamara@hotmail.co.uk>
2025-03-04 19:27:36 +0000

    Update: ADD Selenium Chrome driver

--- MortgageCalculator.Selenium/SeleniumTests.cs
+++ MortgageCalculator.Selenium/SeleniumTests.cs
@@ -1,10 +1,19 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+
 namespace MortgageCalculator.Selenium;
 
 public class Tests
 {
+    private IWebDriver driver;
+
     [SetUp]
     public void Setup()
     {
+        driver = new ChromeDriver();
+        driver.Navigate().GoToUrl("https://example.com");
     }
 
     [Test]
@@ -12,4 +21,10 @@ public class Tests
     {
         Assert.Pass();
     }
+
+    // [TearDown]
+    // public void Cleanup()
+    // {
+    //     driver.Quit(); // Close browser after tests
+    // }
 }
